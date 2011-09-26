918613f7eff986d7bfa509a64daac15d5042e217
Mike Bryant <mikesname@gmail.com>
2011-09-26 19:52:47 +0100

    Improve docstrings

--- nodetree/exceptions.py
+++ nodetree/exceptions.py
@@ -3,24 +3,30 @@ Nodetree exception classes.
 """
 
 class NodeError(Exception):
+    """General nodetree error."""
     def __init__(self, msg, node=None):
         self.message = msg
         self.node = node
         super(NodeError, self).__init__("%s: %s" % (node, msg))
 
-class UnsetParameterError(NodeError):
-    pass
 
 class ValidationError(NodeError):
-    pass
+    """Validation of a node's parameters failed."""
 
-class InvalidParameterError(NodeError):
-    pass
+
+class UnsetParameterError(ValidationError):
+    """A mandatory parameter was not set."""
+
+
+class InvalidParameterError(ValidationError):
+    """A parameter that took a particular type of value, didn't."""
+
 
 class InputOutOfRangeError(NodeError):
-    pass
+    """Attempt to connect an input the node didn't possess."""
+
 
 class CircularDagError(NodeError):
-    pass
+    """A node was connected circularly to its own input."""
 
 
